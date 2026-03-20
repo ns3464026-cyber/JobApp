678cd0428fe76db3bf04b67ac52c66468f339f9a
Nikhil <ns3464026@gmail.com>
2026-03-20 23:34:04 +0530

    jobapp

---
+++ src/main/java/com/example/JobApp/service/JobService.java
@@ -0,0 +1,26 @@
+package com.example.JobApp.service;
+
+import com.example.JobApp.model.JobPost;
+import com.example.JobApp.repo.JobRep;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class JobService {
+
+    @Autowired
+    private JobRep repo;
+
+    public void addJob(JobPost jobPost){
+        repo.addJob(jobPost);
+
+    }
+
+    public List<JobPost> getAllJob(){
+        return repo.getAllJob();
+
+    }
+}
